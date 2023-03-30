988b18f08beebb308a4178ade9cf2b545393aa66
pyran <121752109+pyran19@users.noreply.github.com>
2023-03-30 16:32:53 +0900

    Update moveusage.py
    初期値が悪く、収束性が良くなかったため書き直し

--- moveusage.py
+++ moveusage.py
@@ -10,11 +10,15 @@ moves = range(1, 1+n_move)
 #["ムーンフォース","シャドーボール",...]等技名を並べたリストを作っても可
 
 # 技の使用率を定義 ここをポケモンホームのデータ使って書き換える
-usage_rates = [0.795, 0.511, 0.480, 0.404, 0.312, 0.211, 0.199, 0.162, 0.148, 0.128]
+usage_rates =[0.930,0.900,0.511,0.472,0.394,0.162,0.111,0.090,0.086,0.085]#サーフゴー
+#[0.714,0.691,0.581,0.499,0.255,0.195,0.172,0.170,0.108,0.194]#カイリュー
+#[0.903,0.847,0.842,0.783,0.213,0.198,0.074,0.048,0.026,0.015]#セグ
+#[0.939, 0.928, 0.675, 0.409, 0.223, 0.144, 0.079, 0.078, 0.077, 0.058]#ハバカミ
+
 
 # 技のセットを生成する
 moves_sets = list(itertools.combinations(moves, 4))
-
+ 
 #moves_cmp=[]
 moves_cmp_idx=[]
 for j in range(n_move):
@@ -22,7 +26,6 @@ for j in range(n_move):
     for s in moves_sets:
         if moves[j] in s:
             moves_cmp_idx[j].append(moves_sets.index(s))
-
 
 # 問題の定義
 def objective(x):
@@ -40,6 +43,8 @@ bounds = [(0, 1) for _ in range(len(moves_sets))]
 
 # 初期値
 x0 = np.zeros(len(moves_sets))
+for i,s in enumerate(moves_sets):
+    x0[i] = usage_rates[moves.index(s[0])]*usage_rates[moves.index(s[1])]*usage_rates[moves.index(s[2])]*usage_rates[moves.index(s[3])]
 
 # 最適化
 result = minimize(objective, x0, method='SLSQP', bounds=bounds, constraints=cons)
@@ -49,6 +54,7 @@ sort_idx = np.argsort(result.x)[-1:-n_out-1:-1]
 sorted_x = result.x[sort_idx]
 print_name=[moves_sets[i] for i in sort_idx]
 
+print(f"success?: {result.success}")
 print("型:          使用率")
 for i in range(n_out):
   print(f"{print_name[i]}: {sorted_x[i]:.0%}")
